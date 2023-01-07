055025e73bcc438f97380846b660708810d4b816
Ranjeet Saw <sawranjeet550@gmail.com>
2023-01-07 13:13:03 +0530

    added resultpage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63d55a69e1494eeb"; }
+/******/ 		__webpack_require__.h = function() { return "4a27b1abdced8ba9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
